f12ef7679055da2fd34489a09da4c8969e66ca20
shaan-alam <shaanalam369@gmail.com>
2023-06-28 21:10:22 +0530

    added scripts

---
+++ src/services/jwt.service.js
@@ -0,0 +1,25 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.verifyToken = exports.signToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const SECRET = `${process.env.JWT_SECRET}`;
+const signToken = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield jsonwebtoken_1.default.sign(payload, SECRET);
+});
+exports.signToken = signToken;
+const verifyToken = (token) => __awaiter(void 0, void 0, void 0, function* () {
+    return yield jsonwebtoken_1.default.verify(token, SECRET);
+});
+exports.verifyToken = verifyToken;
